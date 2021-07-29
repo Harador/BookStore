aac35b8cfe2e0a257a9c2ca883455967755168c0
harador <haradorlist@gmail.com>
2021-07-29 17:27:32 +0300

    book-service

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, pipe } from 'rxjs';
+
+// import { tap } from 'rxjs/operators';
+
+import { IBook } from '../interfaces/book.interface';
+import { IAutor } from '../../autor/index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  public books$ = new BehaviorSubject<IBook[]>([]);
+  private booksUrl: string = '/api/books';
+  private subscribe: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.subscribe = this.getBooks().subscribe((books) => {
+      this.books$.next(books);
+      this.subscribe.unsubscribe();
+    })
+  }
+
+  public getBooks(): Observable<IBook[]> {
+    return this.http
+      .get<IBook[]>(this.booksUrl);
+  }
 }
