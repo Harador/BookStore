85820d64a5a0d5af4f87a3f79259caf37517b6ab
harador <haradorlist@gmail.com>
2021-07-29 16:25:18 +0300

    book&genre interfaces

---
+++ src/app/book/interfaces/book.interface.ts
@@ -0,0 +1,14 @@
+import { IGenre } from './genre.interface';
+
+export interface IBook{
+  id: number;
+  description: string;
+  author_id: number;
+  title: string;
+  price: number;
+  genres: IGenre[];
+  previews: string[];
+  image: string|null;
+  writing_date: string;
+  release_date: string;
+}
