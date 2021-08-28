b00a9349e6a10ac3c830e3e68272501884017999
harador <haradorlist@gmail.com>
2021-08-28 11:33:55 +0300

    true-sort-keys helper

---
+++ src/app/helpers/get-true-sort-params.ts
@@ -0,0 +1,32 @@
+import { IFiltration, IPageParams, IQueriesParams } from '../interfaces/queries.interface';
+
+export function getTrueSortParams(params: IFiltration & IPageParams): IQueriesParams {
+  const trueParams: IFiltration = {};
+
+  Object.keys(params).forEach((key) => {
+    switch (key) {
+      case 'genre': {
+        trueParams['q[genres_name_cont]'] = params.genre;
+        break;
+      }
+      case 'authorName': {
+        trueParams['q[name_cont]'] = params.authorName;
+        break;
+      }
+      case 'minPrice': {
+        trueParams['q[price_gteq]'] = params.minPrice;
+        break;
+      }
+      case 'maxPrice': {
+        trueParams['q[price_lteq]'] = params.maxPrice;
+        break;
+      }
+      default: {
+        trueParams[key] = params[key];
+        break;
+      }
+    }
+  });
+
+  return trueParams;
+}
