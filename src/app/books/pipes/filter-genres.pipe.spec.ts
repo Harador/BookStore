d55b55082199c61d2e2abdb623462c06446c9e37
Harador <haradorlist@gmail.com>
2021-08-28 12:18:46 +0300

    delete specs

--- src/app/books/pipes/filter-genres.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterGenresPipe } from './filter-genres.pipe';
-
-describe('FilterGenresPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterGenresPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
